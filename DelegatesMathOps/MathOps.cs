8df36fb7ea7a6cbd5970f4224479c420a228df9e
Michal Stehlík <36696240+MichalStehlik@users.noreply.github.com>
2020-01-27 11:34:43 +0100

    delegates

---
+++ DelegatesMathOps/MathOps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegatesMathOps
+{
+    static class MathOps
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        public static double Divide(double a, double b)
+        {
+            return a / b;
+        }
+
+        public static double SqPower(double a)
+        {
+            return a * a;
+        }
+    }
+
+    public delegate double BinaryOp(double a, double b);
+    public delegate double UnaryOp(double a);
+}
---
+++ DelegatesMathOps/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DelegatesMathOps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Process(1, 2, (x,y) => { return x + y; }));
+            Console.WriteLine(Process(1, (x) => { return x * 2; }));
+        }
+
+        static double Process(double a, double b, BinaryOp fce)
+        {
+            return fce(a, b);
+        }
+
+        static double Process(double a, UnaryOp fce)
+        {
+            return fce(a);
+        }
+    }
+}
